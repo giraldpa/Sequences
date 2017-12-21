14a52686a08f79a1c6a1665d0014161fec169bce
giraldpa <giraldpa@rose-hulman.edu>
2017-12-21 08:37:06 -0500

    DONE

--- src/m6_fancy_iterating.py
+++ src/m6_fancy_iterating.py
@@ -541,7 +541,7 @@ def print_items_that_are_odd_integers(sequence):
       33 is at index 5
     """
     # ------------------------------------------------------------------
-    # TODO: 9. Implement and test this function.
+    # Done: 9. Implement and test this function.
     #
     # IMPORTANT:  The  type  function returns  int  if its argument
     #   is an integer.  Note that   int   has NO quotes surrounding it.
